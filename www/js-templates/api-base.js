12e0d7c61d2a60659e8bad8411513b9c76907335
Amy Boyd <amy@amyboyd.co.uk>
2015-06-13 21:41:27 +0100

    Rename "test" environment to "staging".

--- www/js-templates/api-base.js
+++ www/js-templates/api-base.js
@@ -2,7 +2,7 @@ angular.module('cp-vendor-app.constants', [])
 // @if ENV == 'DEVELOPMENT'
 .constant('API_BASE', 'http://api.citypantry.dev')
 // @endif
-// @if ENV == 'TEST'
+// @if ENV == 'STAGING'
 .constant('API_BASE', 'http://api.staging.citypantry.com')
 // @endif
 // @if ENV == 'PRODUCTION'
